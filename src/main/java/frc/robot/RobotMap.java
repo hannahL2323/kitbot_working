ba0ba5a286c63cb2721febfe3085154254a5c5ca
hannah666L <hannahL2323@users.noreply.github.com>
2023-01-14 12:31:38 -0500

    working kitbot code

---
+++ src/main/java/frc/robot/RobotMap.java
@@ -0,0 +1,14 @@
+
+package frc.robot;
+
+/**
+ * Add your docs here.
+ */
+public class RobotMap {
+
+    public static final int Left1 = 1;
+    public static final int Left2 = 2;
+    public static final int Right1 = 4;
+    public static final int Right2 = 3;
+
+}
